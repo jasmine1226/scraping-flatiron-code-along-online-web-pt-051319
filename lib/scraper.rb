dde2f5ceabe37d16df7103dc0d65d3cd9451adb2
Jasmine Chen <jasmine1226@gmail.com>
2019-06-22 05:52:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,18 +12,16 @@ class Scraper
     end
 
     def get_courses
-      doc = self.get_page
+      self.get_page.css(".post")
+    end
 
+    def make_courses
       doc.css(".post").each do |post|
         course = Course.new
         course.title = post.css("h2").text
         course.schedule = post.css(".date").text
         course.description = post.css("p").text
       end
-    end
-
-    def make_courses
-
     end
 
     def print_courses
