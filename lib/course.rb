a1dd846c7516147a3fd17e17aad82e68121e5803
Jasmine Chen <jasmine1226@gmail.com>
2019-06-22 05:34:13 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,12 +1,13 @@
 class Course
   attr_accessor :title, :schedule, :description
+  @@all = []
 
   def initialize
-    @@courses << self
+    @@all << self
   end
 
   def all
-    @@courses
+    @@all
   end
 
   def reset_all
