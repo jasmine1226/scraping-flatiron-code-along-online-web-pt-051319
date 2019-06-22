b9b7cc802a3d7f31a42dbaffd68b73eea922062d
Jasmine Chen <jasmine1226@gmail.com>
2019-06-22 05:33:54 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,16 @@
+class Course
+  attr_accessor :title, :schedule, :description
 
+  def initialize
+    @@courses << self
+  end
+
+  def all
+    @@courses
+  end
+  
+  def reset_all
+    self.all.clear
+  end
+end
 
